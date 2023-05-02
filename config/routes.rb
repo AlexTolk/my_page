9d377d5453bdd7e8adc686a3d4cd26fafc5d3bb7
Aleksandr Tolkachev <alexandr.tolkachyov@gmail.com>
2023-05-02 19:16:02 +0800

    created index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "pages#index"
+  get "projects", to: "pages#projects"
+  get "about", to: "pages#about"
 end
